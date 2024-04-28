af90251de9ae49c4a3a8efa2860d99fadd00eed8
Talha Asif <imtalha995@gmail.com>
2024-04-28 16:23:52 +0500

    UI and Win count updated

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var userNo = document.getElementById("userNO");
 var RandomNo = document.getElementById("randomNo");
-var ResultInfoPositive = document.getElementById("ResultInfoPositiveId")
-var ResultInfoNegative = document.getElementById("ResultInfoNegativeId")
+var ResultInfoPositive = document.getElementById("ResultInfoPositiveId");
+var ResultInfoNegative = document.getElementById("ResultInfoNegativeId");
+var winCount = document.getElementById("WinsNum");
+
+console.log(winCount);
 
 var chancesNum = document.getElementById("chancesNum");
 var chances = 10;
 var wins = 3;
+winCount.innerHTML = wins;
 
 chancesNum.innerHTML = chances;
 
@@ -22,19 +26,22 @@ function randomNumber(userNumber) {
   if (userNumber == randomNum) {
     wins--;
     console.log(wins, "wins");
+    winCount.innerHTML = wins;
   }
-  if(wins == 0){
-    ResultInfoPositive.className = "unhidden"
-    chances = 10
-    wins = 3
-  }else if(chances == 0){
-    ResultInfoNegative.className = "unhidden"
-    ResultInfoNegative.style.color = "red"
-    chances = 10
-    wins = 3
+  if (wins == 0) {
+    ResultInfoPositive.className = "unhidden";
+    chances = 10;
+    wins = 3;
+  } else if (chances == 0) {
+    ResultInfoNegative.className = "unhidden";
+    ResultInfoNegative.style.color = "red";
+    chances = 10;
+    wins = 3;
   }
-  if(chances == 9){
-    ResultInfoNegative.className = "hidden" 
-    ResultInfoPositive.className = "hidden"
+  if (chances == 9) {
+    ResultInfoNegative.className = "hidden";
+    ResultInfoPositive.className = "hidden";
+    wins = 3
+    winCount.innerHTML = wins 
   }
 }
